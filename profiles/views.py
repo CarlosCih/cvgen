5d5131c7aa051590554946e0571cdff0feae5b0d
CarlosCIh <cihcarlos258@gmail.com>
2026-06-11 17:16:15 -0600

    diseño

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,13 @@
+from django.db.models import Q
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Profile
+
+def profile_view(request):
+    query = request.GET.get('q', '').strip()
+    resumes = Profile.objects.all().order_by('name')
+
+    if query:
+        resumes = resumes.filter(Q(name__icontains=query) | Q(email__icontains=query))
+
+    return render(request, 'main/index.html', {'query': query, 'resumes': resumes})
